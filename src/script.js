c9c766d75466adac2e981e7bb4126eb73eae480f
Kanwar Jot Parkash Singh <52690070+kanwarjotp@users.noreply.github.com>
2025-09-05 23:58:19 +1000

    (feat) parsed pokemon data into lists

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const POKE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
-async function getPokemon( n = 5 ) {
+async function getPokemon( n = 1 ) {
     const pokemons = []
     try {
         for (let i = 1; i <= n; i++) {
@@ -13,14 +13,49 @@ async function getPokemon( n = 5 ) {
 
             const data = await response.json()
             console.log(data)
-            pokemons.push(data)
+            pokemons.push(parsePokemonData(data))
         }
     } catch(error) {
         console.error(error);
     }
 
-    // console.log(pokemons)
+    console.log(pokemons)
+    
 }
+
+function parsePokemonData(pokemonObj) {
+    const name = pokemonObj.name
+    const height = pokemonObj.height
+    const experience = pokemonObj.base_experience
+    const species = pokemonObj.species
+    
+    // TODO: get pokemon image
+    let pokemonImage = ""
+    try{
+        pokemonImage = pokemonObj.sprites.other.official_artwork.front_default 
+
+    } catch (error) {
+        pokemonImage = pokemonObj.sprites.front_default
+    }
+
+    // TODO: get three important points except the demographic values
+    const cries = pokemonObj.cries
+    const abilities = pokemonObj.abilities
+
+    const pokemonData = {
+        name: name,
+        height: height,
+        experience: experience,
+        species: species,
+        pokemonImage: pokemonImage,
+        cries: cries,
+        abilities: abilities
+    }
+    return pokemonData
+}
+
+
+// TODO: create a function to display the pokemon information - or just use Vue?
 
 const getPokemonBtn = document.getElementById("gotta-catch-em")
 // creating an event listener for clicks
