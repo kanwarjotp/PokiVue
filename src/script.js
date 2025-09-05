ab31d36a6b4f9b0293832b0a7eb9e5bfdc89e4fe
Kanwar Jot Parkash Singh <52690070+kanwarjotp@users.noreply.github.com>
2025-09-06 00:29:39 +1000

    (feat) prints pokemons stats, displays image and add an audio for cries

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const POKE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
-async function getPokemon( n = 1 ) {
+async function getPokemon( n = 20 ) {
     const pokemons = []
     try {
         for (let i = 1; i <= n; i++) {
@@ -20,6 +20,9 @@ async function getPokemon( n = 1 ) {
     }
 
     console.log(pokemons)
+
+    // add data to inner html
+    pokemons.forEach((pokemon) => {addHTMLPokemon(pokemon)})
     
 }
 
@@ -46,7 +49,7 @@ function parsePokemonData(pokemonObj) {
         name: name,
         height: height,
         experience: experience,
-        species: species,
+        species: species.name,
         pokemonImage: pokemonImage,
         cries: cries,
         abilities: abilities
@@ -56,6 +59,24 @@ function parsePokemonData(pokemonObj) {
 
 
 // TODO: create a function to display the pokemon information - or just use Vue?
+function addHTMLPokemon(parsedPokemon) {
+    const pokemonCard = `
+        <div>
+        <h3>${parsedPokemon.name}</h3>
+        <img src=${parsedPokemon.pokemonImage}>
+        <audio controls>
+            <source src=${parsedPokemon.cries.latest} type="audio/ogg">
+        </audio>
+        <ul>
+            <li>Height: ${parsedPokemon.height}</li>
+            <li>Experience: ${parsedPokemon.experience}</li>
+            <li>Species: ${parsedPokemon.species}</li>
+            <li>Ability: ${parsedPokemon.abilities[1].ability.name} <sub>(more verbose information to come soon)</sub></li>
+        </ul>
+        </div>`
+    document.getElementById("poke-grid").innerHTML += pokemonCard
+}
+
 
 const getPokemonBtn = document.getElementById("gotta-catch-em")
 // creating an event listener for clicks
