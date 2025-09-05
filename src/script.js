3e08f30c451c2ce0e5c12d0a9ef4a70e398ebc15
Kanwar Jot Parkash Singh <52690070+kanwarjotp@users.noreply.github.com>
2025-09-05 23:38:05 +1000

    (feat) accessed pokemon api - getting the first 10 pokemons in the database

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const POKE_URL = "https://pokeapi.co/api/v2/pokemon/"
+
+async function getPokemon( n = 5 ) {
+    const pokemons = []
+    try {
+        for (let i = 1; i <= n; i++) {
+            console.log("sending a request")
+            var response = await fetch(POKE_URL+(i.toString()))
+
+            if (!response.ok) {
+                throw new error("Could not fetch resource");
+            }
+
+            const data = await response.json()
+            console.log(data)
+            pokemons.push(data)
+        }
+    } catch(error) {
+        console.error(error);
+    }
+
+    // console.log(pokemons)
+}
+
+const getPokemonBtn = document.getElementById("gotta-catch-em")
+// creating an event listener for clicks
+getPokemonBtn.addEventListener('click', getPokemon())
